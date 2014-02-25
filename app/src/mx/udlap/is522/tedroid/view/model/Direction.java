ed70a1a0206e58ba52cd0ee741ef05d0ec2c8836
dan-zx <danyboy_shaggy@hotmail.com>
2014-02-25 10:41:00 -0600

    A @andres-pena-peralta le falto subir este archivo

---
+++ app/src/mx/udlap/is522/tedroid/view/model/Direction.java
@@ -0,0 +1,12 @@
+package mx.udlap.is522.tedroid.view.model;
+
+/**
+ * Constantes de dirección
+ * 
+ * @author Daniel Pedraza-Arcega
+ * @since 1.0
+ */
+public enum Direction {
+    
+    RIGHT, LEFT, DOWN, UP
+}
