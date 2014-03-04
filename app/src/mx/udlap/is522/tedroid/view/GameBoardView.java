93bd5bcf3b545157d4c79f827176da0d8dc6f61e
dan-zx <danyboy_shaggy@hotmail.com>
2014-03-03 23:49:13 -0600

    Revert "Merge branch 'wassin'"
    This reverts commit c16405558a89efe068980030c9370f6a9d180cd2, reversing
    changes made to 27bd252c290086dec1a737b25c3f55e4871a0773.

--- app/src/mx/udlap/is522/tedroid/view/GameBoardView.java
+++ app/src/mx/udlap/is522/tedroid/view/GameBoardView.java
@@ -55,7 +55,6 @@ public class GameBoardView extends View {
     private OnCommingNextTetrominoListener onCommingNextTetrominoListener;
     private OnPointsGainedListener onPointsGainedListener;
     private OnGameOverListener onGameOverListener;
-    private int i=0;
 
     /**
      * Construye un tablero de juego mediante un context.
@@ -97,15 +96,7 @@ public class GameBoardView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(currentTetromino == nextTetromino){
-        	i=i++;
-        }
-        else{i=0;
-        }
-        if(i>3){
-        	nextTetromino =getNextTetromino();
-        	i=0;
-        }
+
         if (!startDropingTetrominos) {
             startDropingTetrominos = true;
             stopDropingTaskIfNeeded();
@@ -114,11 +105,7 @@ public class GameBoardView extends View {
             centerTetromino(currentTetromino);
             if (onCommingNextTetrominoListener != null) onCommingNextTetrominoListener.onCommingNextTetromino(nextTetromino);
             startDropingTask(speed);
-       
         }
-        
-            
-        
 
         currentTetromino.drawOn(canvas);
         drawBoardMatrix(canvas);
