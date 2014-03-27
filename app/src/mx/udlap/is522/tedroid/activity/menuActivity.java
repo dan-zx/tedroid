28ee5b5a9ed8cf37b1244d5328aea7b571f7e598
dan-zx <danyboy_shaggy@hotmail.com>
2014-03-27 11:11:32 -0600

    Pre-merge wassim branch

--- app/src/mx/udlap/is522/tedroid/activity/menuActivity.java
+++ app/src/mx/udlap/is522/tedroid/activity/menuActivity.java
@@ -1,33 +1,32 @@
 package mx.udlap.is522.tedroid.activity;
 
 import android.app.Activity;
-
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.view.View;
 import android.view.View.OnClickListener;
 
 import mx.udlap.is522.tedroid.R;
 
-public class InterfazActivity extends Activity implements OnClickListener {
+public class menuActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.interfaz1);
-        View titulo = findViewById(R.id.titulo);
-        titulo.setOnClickListener((OnClickListener) this);
+        setContentView(R.layout.menu);
+        View game_start = findViewById(R.id.game_start);
+        game_start.setOnClickListener((OnClickListener) this);
+
     }
 
     @Override
     public void onClick(View vista) {
         // TODO Auto-generated method stub
-        if (vista.getId() == findViewById(R.id.titulo).getId()) {
+        if (vista.getId() == findViewById(R.id.game_start).getId()) {
+
             Intent j = new Intent(this, GameActivity.class);
             startActivity(j);
         }
-    }
 
+    }
 }
