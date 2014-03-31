1448eedce32fff5604d579b227af249de3f7bde4
dan-zx <danyboy_shaggy@hotmail.com>
2014-03-31 12:09:42 -0600

    Actualicé el esquema de base de datos

--- app/assets/db/schema-v1.sql
+++ app/assets/db/schema-v1.sql
@@ -10,12 +10,13 @@ CREATE TABLE score (
     is_uploaded_to_google_play BOOLEAN NOT NULL DEFAULT 0
 );
 
-CREATE TABLE pending_achievement (
+CREATE TABLE unlocked_achievement (
 	achievement_id VARCHAR(40) NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE unlocked_achievement (
-	achievement_id VARCHAR(40) NOT NULL PRIMARY KEY
+CREATE TABLE pending_achievement (
+	achievement_id VARCHAR(40) NOT NULL PRIMARY KEY,
+	FOREIGN KEY(achievement_id) REFERENCES unlocked_achievement(achievement_id) ON DELETE CASCADE ON UPDATE CASCADE 
 );
 
 CREATE INDEX score_points_idx ON score (points DESC);
