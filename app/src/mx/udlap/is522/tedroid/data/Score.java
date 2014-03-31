1ad5d78262abff69cce986b1684cc55a38923748
dan-zx <danyboy_shaggy@hotmail.com>
2014-03-30 22:06:12 -0600

    Se corrigieron varios error que podrían hacer fallar las pruebas unitarias.

--- app/src/mx/udlap/is522/tedroid/data/Score.java
+++ app/src/mx/udlap/is522/tedroid/data/Score.java
@@ -11,12 +11,12 @@ import java.util.Date;
  */
 public class Score implements Serializable {
 
-    private static final long serialVersionUID = -4729257445063081681L;
+    private static final long serialVersionUID = 2888499014768693222L;
 
     private Date obtainedAt;
-    private int level;
-    private int lines;
-    private int points;
+    private Integer level;
+    private Integer lines;
+    private Integer points;
 
     public Date getObtainedAt() {
         return obtainedAt;
@@ -57,31 +57,34 @@ public class Score implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + level;
-        result = prime * result + lines;
+        result = prime * result + ((level == null) ? 0 : level.hashCode());
+        result = prime * result + ((lines == null) ? 0 : lines.hashCode());
         result = prime * result + ((obtainedAt == null) ? 0 : obtainedAt.hashCode());
-        result = prime * result + points;
+        result = prime * result + ((points == null) ? 0 : points.hashCode());
         return result;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null) return false;
         if (getClass() != obj.getClass()) return false;
         Score other = (Score) obj;
-        if (level != other.level) return false;
-        if (lines != other.lines) return false;
+        if (level == null) {
+            if (other.level != null) return false;
+        } else if (!level.equals(other.level)) return false;
+        if (lines == null) {
+            if (other.lines != null) return false;
+        } else if (!lines.equals(other.lines)) return false;
         if (obtainedAt == null) {
             if (other.obtainedAt != null) return false;
         } else if (!obtainedAt.equals(other.obtainedAt)) return false;
-        if (points != other.points) return false;
+        if (points == null) {
+            if (other.points != null) return false;
+        } else if (!points.equals(other.points)) return false;
         return true;
     }
-
+    
     /**
      * {@inheritDoc}
      */
