79083459d5fa10fbc724a135d8a1a5f42c403e87
dan-zx <danyboy_shaggy@hotmail.com>
2014-04-01 12:05:53 -0600

    Progamé el desbloqueo de la mayor parte logros

--- app/src/mx/udlap/is522/tedroid/activity/GameActivity.java
+++ app/src/mx/udlap/is522/tedroid/activity/GameActivity.java
@@ -1,7 +1,6 @@
 package mx.udlap.is522.tedroid.activity;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
@@ -18,6 +17,10 @@ import mx.udlap.is522.tedroid.data.dao.ScoreDAO;
 import mx.udlap.is522.tedroid.view.GameBoardView;
 import mx.udlap.is522.tedroid.view.NextTetrominoView;
 import mx.udlap.is522.tedroid.view.model.Tetromino;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Actividad principal del juego donde se puede jugar realmente.
@@ -96,7 +99,7 @@ public class GameActivity extends BaseGameActivity {
                 newScore.setLevel(level);
                 newScore.setLines(totalLines);
                 newScore.setPoints(score);
-                new ScoreSaver(GameActivity.this).execute(newScore);
+                new ScoresAsyncTask().execute(newScore);
             }
         });
         restartDialog = new AlertDialog.Builder(this)
@@ -236,7 +239,8 @@ public class GameActivity extends BaseGameActivity {
     }
 
     /**
-     * Actualiza el puntaje del juego cuando hay lineas borradas.
+     * Actualiza el puntaje del juego cuando hay lineas borradas y desbloquea
+     * logros.
      * 
      * @param linesCleared las lineas borradas.
      */
@@ -246,27 +250,41 @@ public class GameActivity extends BaseGameActivity {
             case 1: factor = 40; break;
             case 2: factor = 100; break;
             case 3: factor = 300; break;
-            case 4: 
-                factor = 1200;
-                unlockAchievement(R.string.in_a_row_achievement_id);
-                break;
+            case 4: factor = 1200; unlockAchievement(R.string.in_a_row_achievement_id); break;
             default: factor = 1; break;
         }
         
         score += factor * (level + 1);
+        if (score >= 999999) unlockAchievement(R.string.believe_it_or_not_achievement_id);
     }
 
     /**
-     * Actualiza el nivel del juego al checar las lineas borradas.
+     * Actualiza el nivel del juego al checar las lineas borradas y desbloquea
+     * logros.
      * 
      * @param linesCleared las lineas borradas.
      * @param si se subio de nivel o no.
      */
     private boolean updateLevelIfNeeded(int linesCleared) {
-        boolean mayLevelUp = totalLines % 10 >= 6;
+        boolean shouldLevelUp = totalLines % 10 >= 6;
         totalLines += linesCleared;
-        if (mayLevelUp && totalLines % 10 <= 3) {
+        if (totalLines >= 9999) unlockAchievement(R.string.like_a_boss_achievement_id);
+        if (shouldLevelUp && totalLines % 10 <= 3) {
             level++;
+            if (level >= 10) unlockAchievement(R.string.whats_next_achievement_id);
+            else {
+                switch(level) {
+                    case 1: unlockAchievement(R.string.for_dummies_achievement_id); break;
+                    case 2: unlockAchievement(R.string.as_easy_as_pie_achievement_id); break;
+                    case 3: unlockAchievement(R.string.beginner_achievement_id); break;
+                    case 4: unlockAchievement(R.string.amateur_achievement_id); break;
+                    case 5: unlockAchievement(R.string.expert_achievement_id); break;
+                    case 6: unlockAchievement(R.string.master_achievement_id); break;
+                    case 7: unlockAchievement(R.string.pro_achievement_id); break;
+                    case 8: unlockAchievement(R.string.pro_plus_plus_achievement_id); break;
+                    case 9: unlockAchievement(R.string.lucky_you_achievement_id); break;
+                }
+            }
             return true;
         }
 
@@ -292,29 +310,39 @@ public class GameActivity extends BaseGameActivity {
     }
 
     /**
-     * Tarea asíncrona para guardar puntajes. 
-     * (TODO: debe estar en otro lado esta clase)
+     * Tarea asíncrona para guardar puntajes y revisar si hay logros a desbloquear.
      *  
      * @author Daniel Pedraza-Arcega
      * @since 1.0
      */
-    private static class ScoreSaver extends AsyncTask<Score, Void, Void> {
+    private class ScoresAsyncTask extends AsyncTask<Score, Void, List<Integer>> {
 
         private ScoreDAO scoreDAO;
-
-        /**
-         * Crea una nueva tarea asíncrona.
-         * 
-         * @param context el contexto de la aplicación.
-         */
-        public ScoreSaver(Context context) {
-            scoreDAO = new DAOFactory(context.getApplicationContext()).get(ScoreDAO.class);
+        
+        @Override
+        protected void onPreExecute() {
+            scoreDAO = new DAOFactory(getApplicationContext()).get(ScoreDAO.class);
         }
 
         @Override
-        protected Void doInBackground(Score... params) {
-            for (Score score : params) scoreDAO.save(score);
-            return null;
+        protected List<Integer> doInBackground(Score... score) {
+            scoreDAO.save(score[0]);
+            ArrayList<Integer> unlockedAchievements = new ArrayList<Integer>();
+            Map<String, Integer> sums = scoreDAO.readSumOfLinesAndPoints();
+            int linesSum = sums.get("lines_sum");
+            int pointsSum = sums.get("points_sum");
+            if (linesSum >= 9999) unlockedAchievements.add(R.string.nothing_to_do_achievement_id);
+            if (linesSum >= 999999) unlockedAchievements.add(R.string.get_a_life_achievement_id);
+            if (pointsSum >= 9999) unlockedAchievements.add(R.string.boooooring_achievement_id);
+            if (pointsSum >= 999999) unlockedAchievements.add(R.string.tenacious_achievement_id);
+            return unlockedAchievements;
+        }
+
+        @Override
+        protected void onPostExecute(List<Integer> unlockedAchievements) {
+            for (int id : unlockedAchievements) {
+                unlockAchievement(id);
+            }
         }
     }
 }
